65690a006de399f4cdeafb2fc1ee11c85b323842
Daniel Geneti <genetidaniel281@gmail.com>
2023-02-16 19:35:54 -0500

    Initial commit

---
+++ src/ArraySum.java
@@ -0,0 +1,9 @@
+public class ArraySum {
+    //5, 6, 7
+    //5 + index+1
+    public int sumOfArray (Integer[] a,int index){
+        if(index < 0)return 0;
+
+        return a[index] + sumOfArray( a, index-1);
+    }
+}
