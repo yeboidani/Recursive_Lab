a3c20e8530656d52a3d76404915d55086aab2fe5
Daniel Geneti <genetidaniel281@gmail.com>
2023-02-16 19:38:53 -0500

    Final Change

--- src/ArraySum.java
+++ src/ArraySum.java
@@ -4,6 +4,6 @@ public class ArraySum {
     public int sumOfArray (Integer[] a,int index){
         if(index < 0)return 0;
 
-        return a[index] + sumOfArray( a, index);
+        return a[index] + sumOfArray( a, index-1);
     }
 }
